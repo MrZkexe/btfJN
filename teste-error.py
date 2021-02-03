b56f5ba8fa85a74acb5fab15db10cb6c627fc224
Mr-Zk_exe <78140321+MrZkexe@users.noreply.github.com>
2021-02-03 06:52:37 -0300

    Delete teste-error.py

--- teste-error.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = input('link: ')
-userdata = input('usuario data: ')
-passdata = input('senha data: ')
-inf = {userdata : 'jbxzjcbdsjbcjdsbcjdbsbcjsdbcbsdjcb', passdata : 'jbxzjcbdsjbcjdsbcjdbsbcjsdbcbsdjcb'}
-rs = requests.post(url, data=inf)
-print(rs.text)

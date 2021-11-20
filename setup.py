c2319973fe2a53b7390e6ee450817e23a5487a3f
Mr-Zk_exe <78140321+MrZkexe@users.noreply.github.com>
2021-11-20 00:56:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name='btfJN',
     author='Zk_exe',
-	version='3.5.0',
+	version='4.0.0',
 	packages=['btf'],
 	description='script para bruteforce em sites, criado por zk_exe',
     install_requires=[
 		'requests',
+		'climage',
 	],
 )
